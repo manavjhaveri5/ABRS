75bf79352d37e74b995a00099ccfe75801cf44de
manavjhaveri5 <manav08jhaveri@hotmail.com>
2024-11-13 14:36:46 -0500

    Update TEST-limit_switch.py

--- TEST-limit_switch.py
+++ TEST-limit_switch.py
@@ -1,21 +1,22 @@
 """
 This Raspberry Pi code was developed by newbiely.com
-This Raspberry Pi code is made available for public use without any restriction.
+This Raspberry Pi code is made available for public use without any restriction
 For comprehensive instructions and wiring diagrams, please visit:
 https://newbiely.com/tutorials/raspberry-pi/raspberry-pi-limit-switch
 """
 
+
 import RPi.GPIO as GPIO
-import time
 
 # Set the GPIO mode to BCM
 GPIO.setmode(GPIO.BCM)
 
 # Define the GPIO pin for your button
-SWITCH_PIN = 26  # or 19 for SWITCH 1
+#SWITCH_PIN = 19 #SWITCH 1
+SWITCH_PIN = 26 #SWITCH 2
 
 # Define debounce time in milliseconds
-DEBOUNCE_TIME_MS = 300  # Increase debounce to 300 milliseconds
+DEBOUNCE_TIME_MS = 300  # 200 milliseconds
 
 # Set the initial state and pull-up resistor for the button
 GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -28,7 +29,6 @@ prev_switch_state = switch_state
 def button_callback(channel):
     global switch_state
     switch_state = GPIO.input(SWITCH_PIN)
-    print(f"Callback triggered, current switch state: {'UNTOUCHED' if switch_state == GPIO.HIGH else 'TOUCHED'}")
 
 # Add an event listener for the button press
 GPIO.add_event_detect(SWITCH_PIN, GPIO.BOTH, callback=button_callback, bouncetime=DEBOUNCE_TIME_MS)
@@ -45,8 +45,11 @@ try:
             
             prev_switch_state = switch_state
 
-        # Small delay in main loop to avoid flooding the terminal
-        time.sleep(0.2)
+
+        if switch_state == GPIO.HIGH:
+            print("The limit switch: UNTOUCHED")
+        else:
+            print("The limit switch: TOUCHED")
 
 except KeyboardInterrupt:
     # Clean up GPIO on exit
