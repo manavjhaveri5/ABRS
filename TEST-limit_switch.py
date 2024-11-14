10a113e5670bcbe766b92b5ca9b45e55a84f26e8
manavjhaveri5 <manav08jhaveri@hotmail.com>
2024-11-14 15:36:00 -0500

    Update TEST-limit_switch.py

--- TEST-limit_switch.py
+++ TEST-limit_switch.py
@@ -16,13 +16,13 @@ try:
         if GPIO.input(LIMIT_SWITCH_1_PIN) == GPIO.LOW:
             print("Limit Switch 1: Touched")
         else:
-            print("Limit Switch 1: Untouched")
+            #print("Limit Switch 1: Untouched")
         
         # Check limit switch 2
         if GPIO.input(LIMIT_SWITCH_2_PIN) == GPIO.LOW:
             print("Limit Switch 2: Touched")
         else:
-            print("Limit Switch 2: Untouched")
+            #print("Limit Switch 2: Untouched")
         
         time.sleep(0.5)  # Adjust delay as needed
 
